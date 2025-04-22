8e96dcebd0e6abad41233dde3ba143c6cbd37400
Aaron Dodd <ADODD300@caledonian.ac.uk>
2025-04-22 22:28:34 +0100

    Fix UserDAO to safely create users.db for Render

--- models/userModel.js
+++ models/userModel.js
@@ -1,56 +1,61 @@
 const Datastore = require("gray-nedb");
-const bcrypt = require('bcryptjs');
+const path = require("path");
+const fs = require("fs");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 class UserDAO {
-    constructor(dbFilePath) {
-        if (dbFilePath) {
-            this.db = new Datastore({ filename: dbFilePath,
-            autoload: true });
-        } else {
-            this.db = new Datastore();
-        }
+  constructor(dbFilePath) {
+    const resolvedPath = dbFilePath || path.join(__dirname, "../db/users.db");
+
+    const dbDir = path.dirname(resolvedPath);
+    if (!fs.existsSync(dbDir)) {
+      fs.mkdirSync(dbDir, { recursive: true });
     }
-    init() {
-        this.db.insert({
-            user: 'Peter',
-            password:
-            '$2b$10$I82WRFuGghOMjtu3LLZW9OAMrmYOlMZjEEkh.vx.K2MM05iu5hY2C'
-        });
-        this.db.insert({
-            user: 'Ann',
-            password: '$2b$10$bnEYkqZM.MhEF/LycycymOeVwkQONq8kuAUGx6G5tF9UtUcaYDs3S'
-        });
-        return this;
+
+    if (!fs.existsSync(resolvedPath)) {
+      fs.writeFileSync(resolvedPath, "");
     }
-    create(username, password) {
-        const that = this;
-        bcrypt.hash(password, saltRounds).then(function(hash) {
-            var entry = {
-                user: username,
-                password: hash,
-            };
-            that.db.insert(entry, function (err) {
-            if (err) {
-            console.log("Can't insert user: ", username);
-            }
-            });
-        });
-    }
-    lookup(user, cb) {
-        this.db.find({'user': user}, function (err, entries) {
+
+    this.db = new Datastore({ filename: resolvedPath, autoload: true });
+  }
+
+  init() {
+    this.db.insert({
+      user: "Peter",
+      password: "$2b$10$I82WRFuGghOMjtu3LLZW9OAMrmYOlMZjEEkh.vx.K2MM05iu5hY2C",
+    });
+    this.db.insert({
+      user: "Ann",
+      password:
+        "$2b$10$bnEYkqZM.MhEF/LycycymOeVwkQONq8kuAUGx6G5tF9UtUcaYDs3S",
+    });
+    return this;
+  }
+
+  create(username, password) {
+    const that = this;
+    bcrypt.hash(password, saltRounds).then(function (hash) {
+      const entry = {
+        user: username,
+        password: hash,
+      };
+      that.db.insert(entry, function (err) {
         if (err) {
-            return cb(null, null);
-        } else {
-            if (entries.length == 0) {
-                return cb(null, null);
-            }
-                return cb(null, entries[0]);
-            }
-        });
-    }
+          console.log("Can't insert user: ", username);
+        }
+      });
+    });
+  }
+
+  lookup(user, cb) {
+    this.db.find({ user: user }, function (err, entries) {
+      if (err || entries.length === 0) {
+        return cb(null, null);
+      }
+      return cb(null, entries[0]);
+    });
+  }
 }
-// const dao = new UserDAO("./db/users.db");
-// dao.init();
 
 module.exports = UserDAO;
